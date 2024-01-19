03dff3b367280bf173a4d02c6729cd1f8ff58390
Fathima PA <1968fathimapa@gmail.com>
2024-01-19 12:57:02 +0530

    done

--- palettepassion/settings.py
+++ palettepassion/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY_ID')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOST')
+ALLOWED_HOSTS = [config('ALLOWED_HOST')]
 
 
 # Application definition
